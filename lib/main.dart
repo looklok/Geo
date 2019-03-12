f2ca4e131d15b04993ce0fabb67b6f94aefca5fe
yacine <yacine.brih@gmail.com>
2019-03-12 20:13:16 +0100

    la version finale de lapplication du chauffeur

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -31,7 +33,6 @@ class _MyHomePageState extends State<MyHomePage> {
   String adress_time_seconde = '00.00';
   String adress_time_heure = '00.00';
   String adress_time_minute = '00.00';
-
   String adress_speed = '00.00';
   String localite='';
   bool connected = false;
@@ -39,49 +40,73 @@ class _MyHomePageState extends State<MyHomePage> {
   String wilaya='';
   StreamSubscription<Position> positionStream ;
   double distance=0;
+  double busID = 1;
+  double ligne = 1;
+  bool first = true;
+
 
   Future getadress () async{
-    int i =0;
-    /*Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
-    adress_longitude = position.longitude.toString();
-    adress_latitude = position.latitude.toString();
-    adress_time = position.timestamp.second.toString();
-    adress_speed = position.speed.toString();
-    print('$adress_latitude $adress_longitude $adress_altitude');
-    List<Placemark> placemark = await Geolocator().placemarkFromCoordinates(position.latitude, position.longitude);
-    localite = placemark[0].locality;
-    wilaya = placemark[0].administrativeArea;*/
+
+    var busref = Firestore.instance.collection('bus').document('bus '+busID.toString());
+    List<DocumentSnapshot> l;
+    /*var ref = Firestore.instance.collection('bus');
+    var query = ref.getDocuments();
+    query.then((QuerySnapshot q) {
+      q.documents.forEach((DocumentSnapshot v) {
+        int.parse(v.documentID);
+      });
+    });*/
     if (connected == true) {
-      print('iiiii iiiiiiii');
+      /* writing to database */
+      Map<String ,dynamic> data = Map();
+      data['ligne']=ligne;
+      data['busID'] = busID;
+      data['actif'] = true;
       var locationOptions = LocationOptions(accuracy: LocationAccuracy.bestForNavigation, distanceFilter: 10);
-
-      positionStream = Geolocator().getPositionStream(locationOptions).listen((Position position1)
+      positionStream =Geolocator().getPositionStream(locationOptions).listen((Position position1) async
           {
-
-                  print(i);
-                  i++;
-                  print(position1 == null ? 'Unknown' : position1.latitude.toString() + ', ' + position1.longitude.toString());
+                  print(position1 == null ? 'Unknown' : position1.latitude.toString() + ', ' + position1.speed.toString());
                   adress_longitude = position1.longitude.toString();
                   adress_latitude = position1.latitude.toString();
-                  adress_speed=(position1.speed*1.85).toString();
+                  adress_speed=(position1.speed*3.6).toString();
                   adress_time_seconde = position1.timestamp.second.toString();
                   adress_time_minute = position1.timestamp.minute.toString();
                   adress_time_heure = position1.timestamp.hour.toString();
-                  print(position1.speedAccuracy);
+                  data['latitude'] = position1.latitude;
+                  data['longitude'] = position1.longitude;
+                  data['vitesse'] = (position1.speed*3.6);
+                  if (first == true) {data['prochain']=2; first= false;}
+                  else {
+                    /// récuperer l'arret prochain de ce bus
+                    var query = await Firestore.instance.collection('bus').where('busID',isEqualTo: busID).getDocuments();
+                     int prochain = query.documents[0].data['prochain'];
+                     print('prochain  : '+prochain.toString());
+                     var Ligne = await Firestore.instance.collection('lignes').where('ligneID' , isEqualTo: ligne).getDocuments();
+                     ///récuperer le nombre d'arrets de bus de ce bus
+                     var nb = Ligne.documents[0].data['nb_arrets'];
+                     var query1 = await Ligne.documents[0].reference.collection('arrets de bus').where('arretID', isEqualTo: prochain).getDocuments();
+                     ///récuperer latitude et longitude du bus
+                     var lat = query1.documents[0].data['latitude'];
+                     var lon = query1.documents[0].data['longitude'];
+                     /// calculer la distance entre le bus et l'arret prochain
+                     var dis = await Geolocator().distanceBetween(lat.toDouble(), lon.toDouble(), position1.latitude, position1.longitude);
+                      print('la distance est : '+dis.toString());
+                      if (dis < 25){if (prochain == nb ){ data['prochain'] = 2;} else {data['prochain']= prochain+1;}}
+                      else {data['prochain']=prochain;}
+
+                  }
+                  busref.setData(data);
                   Geolocator().distanceBetween(36.745664, 3.069634, position1.latitude, position1.longitude).then((double d){
                     distance=d;
                   });
-
                   setState(() {});
-
           });
-    }else {
+    }
+    else {
       print('heeeeeeeere');
+      busref.updateData({'actif' : false});
       positionStream.cancel();
-      setState(() {
-
-      });
-
+      setState(() {});
     }
   }
 
@@ -94,17 +119,17 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   void connect ()
   {
-    if (connected==false){connected = true;
-    siconnecte = 'ETAT :connecte';
-    recuperer_adr();
-    setState(() {
-
-    });}
+    if (connected==false) {
+        connected = true;
+        siconnecte = 'ETAT :connecte';
+        recuperer_adr();
+        setState(() {});
+          }
     else {
       connected = false;
       siconnecte = 'ETAT : pas connecte';
       print('stream cancelled');
-      positionStream.cancel();
+      recuperer_adr();
       setState(() {});
           }
     }
@@ -114,13 +139,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -144,7 +165,6 @@ class _MyHomePageState extends State<MyHomePage> {
               '$adress_latitude',
               style: Theme.of(context).textTheme.display1,
             ),
-
             Text(
               'votre vitesse est : ',
             ),
@@ -174,7 +194,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
